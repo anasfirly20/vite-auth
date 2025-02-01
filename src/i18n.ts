3a892bc052e4992854c2a1c3ec7c33a24dfb1483
Anas Firly <anasfirly20@gmail.com>
2025-02-01 09:26:21 +0300

    chore: setup internalization

---
+++ src/i18n.ts
@@ -0,0 +1,29 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    supportedLngs: ["en", "ru"],
+    ns: ["common"],
+    defaultNS: "common",
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ["cookie", "localStorage", "navigator"],
+      caches: ["cookie"],
+      cookieMinutes: 10080,
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
+  });
+
+export default i18n;
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,17 +1,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
+import { I18nextProvider } from "react-i18next";
 
 import { AuthProvider } from "./auth-provider";
 import { ThemeProvider } from "./theme-provider";
+
+import i18n from "@/i18n";
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="light">
-        <AuthProvider>{children}</AuthProvider>
-      </ThemeProvider>
+      <I18nextProvider i18n={i18n}>
+        <ThemeProvider defaultTheme="light">
+          <AuthProvider>{children}</AuthProvider>
+        </ThemeProvider>
+      </I18nextProvider>
     </QueryClientProvider>
   );
 }
